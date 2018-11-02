898969a2671ce5830de1a85c0e5722c1f2fd6a7e
Majakubo <m.jakubowski.git@gmail.com>
2018-11-02 20:20:54 +0100

    added user authentication and parsing request in put endpoint

--- RESTful/app.py
+++ RESTful/app.py
@@ -1,16 +1,23 @@
 from flask import Flask
-from flask_restful import Resource, Api, request
+from flask_restful import Resource, Api, request, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authentication, identity
 
 app = Flask(__name__)
+app.secret_key = 'super_super_secret_key'
 api = Api(app)
+
+jwt = JWT(app, authentication, identity)
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # instead using list(filter()) we use next(filter()) to get first item from filter
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
+    
     
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
@@ -20,17 +27,29 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 200
-
+    
+    
     def put(self, name):
-        data = request.get_json(force=True)
-        price = data['price']
-        for item in items:
-            if item['name'] == name:
-                item['price'] = price
-                return item
-        new_item = {'name': name, 'price': price}
-        items.append(new_item)
-        return new_item
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+                type=float,
+                required=True,
+                help="This field cannot be left blank!"
+        )
+        data = parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+
+        return item
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
 
 class ItemList(Resource):
     def get(self):
